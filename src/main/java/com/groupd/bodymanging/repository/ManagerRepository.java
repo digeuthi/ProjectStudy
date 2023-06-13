d4fc7c9d4c4d4509a11ed85d809d153883bf2055
Dahye Kim <digeuthi@gmail.com>
2023-06-13 15:18:16 +0900

    repository

---
+++ src/main/java/com/groupd/bodymanging/repository/ManagerRepository.java
@@ -0,0 +1,5 @@
+package com.groupd.bodymanging.repository;
+
+public interface ManagerRepository {
+    
+}
--- src/main/java/com/groupd/bodymanging/repository/UserRepository.java
+++ src/main/java/com/groupd/bodymanging/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.groupd.bodymanging.entity.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
-    
+                    //UserEntity의 PK가 int 타입이므로 Integer타입으로 저장함
     public boolean existsByEmail(String userEmail);
     public boolean existsByNickname(String userNickname);
     public boolean existsByPhoneNumber(String userPhoneNumber);
